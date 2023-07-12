7ed15136d315c95dbf509ce4745ee5e026a7b1e4
joohyeona <joohyeon505@gmail.com>
2023-07-12 19:58:00 +0900

    home page front push

--- build/config/urls.py
+++ build/config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from chatbot import views
 
 # 슬래시 붙이기 - url 정규화
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cinemabot/', views.index),
+    path('cinemabot/', include('chatbot.urls')),
 
 ]
