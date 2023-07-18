eaa8d126b9681efc78e811ee288348dc0e5e3177
Hyeon <125099909+joohyeona@users.noreply.github.com>
2023-07-18 10:49:51 +0900

    Revert "Features/backend"

--- web/chatbot/urls.py
+++ web/chatbot/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('talk/', views.talk_view, name='talk-button'),
-    path('survey/', views.survey_view, name='survey-button'),
-    path('suggest/', views.suggest_view, name='suggest-button'),
-
+    path('', views.my_view, name='my-view'),
+    
 ]
--- web/chatbot/views.py
+++ web/chatbot/views.py
@@ -3,19 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
+def my_view(request):
     #context = {}
     #return HttpResponse("...")
     return render(request, 'frontend/home.html')
-
-def talk_view(request):
-    #context = {}
-    return render(request, 'frontend/talk.html')
-
-def survey_view(request):
-    #context = {}
-    return render(request, 'frontend/survey.html')
-
-def suggest_view(request):
-    #context = {}
-    return render(request, 'frontend/suggest.html')
